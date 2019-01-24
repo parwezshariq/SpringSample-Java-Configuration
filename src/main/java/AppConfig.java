ff540d7b3c187e8333dbb3daa573dbf61b535d7c
shariqparwez <shariqparwez@outlook.com>
2019-01-24 11:46:28 +0530

    Using properties file

--- src/main/java/AppConfig.java
+++ src/main/java/AppConfig.java
@@ -1,6 +1,8 @@
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
 
 import com.shariqparwez.repository.CustomerRepository;
 import com.shariqparwez.repository.HibernateCustomerRepositoryImpl;
@@ -9,7 +11,13 @@ import com.shariqparwez.service.CustomerServiceImpl;
 
 @Configuration
 @ComponentScan({"com.shariqparwez"})
+@PropertySource("app.properties")
 public class AppConfig {
+	
+	@Bean
+	public static PropertySourcesPlaceholderConfigurer getPropertySourcesPlaceholderConfigurer() {
+		return new PropertySourcesPlaceholderConfigurer();
+	}
 	
 	/*@Bean(name = "customerService")
 	public CustomerService getCustomerService() {
--- src/main/java/com/shariqparwez/repository/HibernateCustomerRepositoryImpl.java
+++ src/main/java/com/shariqparwez/repository/HibernateCustomerRepositoryImpl.java
@@ -3,6 +3,7 @@ package com.shariqparwez.repository;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Repository;
 
 import com.shariqparwez.model.Customer;
@@ -10,11 +11,12 @@ import com.shariqparwez.model.Customer;
 @Repository("customerRepository")
 public class HibernateCustomerRepositoryImpl implements CustomerRepository {
 	
-	/* (non-Javadoc)
-	 * @see com.shariqparwez.repository.CustomerRepository#findAll()
-	 */
+	@Value("${dbUsername}")
+	private String dbUsername;
+	
 	@Override
 	public List<Customer> findAll() {
+		System.out.println(dbUsername);
 		List<Customer> customers = new ArrayList<>();
 		
 		Customer customer = new Customer();
